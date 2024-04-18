d566155d8de538f8e8df79d8004438c1afd4fa66
polpuigdemont <polpuigdemont@gmail.com>
2024-04-18 15:27:27 +0200

    mod: nvim-tree keymaps

--- nvim/lua/core/plugin_config/nvim-tree.lua
+++ nvim/lua/core/plugin_config/nvim-tree.lua
@@ -3,4 +3,6 @@ vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup()
 
-vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+-- set the command to go to the tree when opened to be leader rt
+vim.api.nvim_set_keymap('n', '<leader>rt', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
