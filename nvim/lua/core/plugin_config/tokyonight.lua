48576774d2ddf966c706153cd1a759cd02fd5a12
polpuigdemont <polpuigdemont@gmail.com>
2024-08-26 19:20:06 +0200

    day and nite

--- nvim/lua/core/plugin_config/tokyonight.lua
+++ nvim/lua/core/plugin_config/tokyonight.lua
@@ -1,8 +1,28 @@
-require('tokyonight').setup({ 
-    style="day",
-    -- no italic for functions
-    styles = {
-        functions = {}
-    },
-})
-vim.cmd[[colorscheme tokyonight]]
+-- Define a function to set the theme based on the time of day
+local function set_tokyonight_style()
+    local hour = tonumber(os.date("%H"))
+    
+    if hour >= 19 or hour < 5 then
+        -- After 8 PM or before 6 AM, use the night style
+        require('tokyonight').setup({
+            style = "night",
+            styles = {
+                functions = {}
+            },
+        })
+    else
+        -- Between 6 AM and 8 PM, use the day style
+        require('tokyonight').setup({
+            style = "day",
+            styles = {
+                functions = {}
+            },
+        })
+    end
+    
+    -- Apply the colorscheme
+    vim.cmd[[colorscheme tokyonight]]
+end
+
+-- Set the theme when Neovim starts
+set_tokyonight_style()
