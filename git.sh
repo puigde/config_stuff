b7fc9987c3e570f772a144a264f13168d2db6fd0
polpuigdemont <polpuigdemont@gmail.com>
2025-04-03 10:11:29 +0200

    auto update

--- git.sh
+++ git.sh
@@ -6,3 +6,11 @@ gpm() {
     git push origin main;
     echo "updated to origin main";
 }
+
+function gds() {
+  local tmpfile
+  tmpfile=$(mktemp)
+  git ls-files > "$tmpfile"
+  tree --fromfile "$tmpfile"
+  rm "$tmpfile"
+}
